2dabd970d91cc35a1616e77ee988a06b709e45ae
Özgür Tokay <ozytky@gmail.com>
2022-02-10 01:00:14 +0300

    j1- arrays2 uygulama3

---
+++ 0-ilk/src/j1/arrays2.java
@@ -0,0 +1,24 @@
+package j1;
+
+import java.util.Scanner;
+
+public class arrays2 {
+    public static void main(String[] args) {
+        /*
+         * Kullanıcıdan "n" tane değer alın. Bu "n" tane değeri bir dizide saklayın ve
+         * bunların ortalamasını ekrana yazdırın
+         */
+
+        Scanner girdiAl = new Scanner(System.in);
+        System.out.print("n: ");
+        int n = girdiAl.nextInt(); // değeri değişkende tut
+
+        int[] dizi = new int[n];
+        int toplam = 0;
+        for (int i = 0; i < n; i++) {
+            dizi[i] = girdiAl.nextInt();
+            toplam += dizi[i];
+        }
+        System.out.println(toplam / n);
+    }
+}
