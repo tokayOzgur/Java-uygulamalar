b890a8f0f8100fcecf10d6b59480750e61737d27
Özgür Tokay <ozytky@gmail.com>
2022-02-24 23:58:37 +0300

    Create oop0_giris.java

---
+++ 0-ilk/src/j2OOP/oop0_giris.java
@@ -0,0 +1,20 @@
+package j2OOP;
+
+public class oop0_giris {
+    public static void main(String[] args) {
+
+    }
+
+    public class Dikdortgen {
+        int en;
+        int boy;
+
+        public Dikdortgen() {
+
+        }
+
+        public int alanHesapla() {
+            return en * boy;
+        }
+    }
+}
