02cd2b393c5a3bbbde0fa0561f507bc01b8eb938
Özgür Tokay <ozytky@gmail.com>
2022-02-25 15:07:03 +0300

    OOP Immutable

---
+++ 0-ilk/src/j2OOP/oop1Immutable.java
@@ -0,0 +1,46 @@
+package j2OOP;
+
+public class oop1Immutable {
+    public static void main(String[] args) {
+        // Your code
+        String[] firmalaStrings = { "Lenovo", "Inel", "AMD" };
+        Bilgisayar b1 = new Bilgisayar(8, 16, firmalaStrings);
+        Bilgisayar b2 = new Bilgisayar(8, 16, new String[] { "Lenovo", "Inel", "AMD" });
+        bilgisayariYazdir(b1);
+    }
+
+    public static void bilgisayariYazdir(Bilgisayar pc) {
+        System.out.println("\nÇekirdek Sayısı: " + pc.getCekirdekSayisi() + "\nRam Miktarı: " + pc.getRamBoyutu()
+                + "\nUretici Firmalar: " + pc.getUreticiFirmalar()[0]);
+    }
+}
+
+/*
+ * Immutable
+ * Rule1- Tüm alanlar private olmalıdır.
+ */
+
+class Bilgisayar {
+    private int cekirdekSayisi;
+    private int ramBoyutu;
+    private String[] ureticiFirmalar;
+
+    public Bilgisayar(int cekirdekSayisi, int ramBoyutu, String[] ureticiFirmalar) {
+        this.cekirdekSayisi = cekirdekSayisi;
+        this.ramBoyutu = ramBoyutu;
+        this.ureticiFirmalar = ureticiFirmalar;
+    }
+
+    public int getCekirdekSayisi() {
+        return cekirdekSayisi;
+    }
+
+    public int getRamBoyutu() {
+        return ramBoyutu;
+    }
+
+    public String[] getUreticiFirmalar() {
+        return ureticiFirmalar;
+    }
+
+}
