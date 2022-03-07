55dbbfe81bcae9d8d2bd6ca76330c4131a61a434
Özgür Tokay <ozytky@gmail.com>
2022-03-07 23:23:25 +0300

    Update j3_linked_hashmap.java

--- 0-ilk/src/j5_collection/j3_linked_hashmap.java
+++ 0-ilk/src/j5_collection/j3_linked_hashmap.java
@@ -1,5 +1,30 @@
 package j5_collection;
 
-public class j3_linked_hashmap {
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.Map.Entry;
 
+public class j3_linked_hashmap {
+	public static void main(String[] args) {
+		LinkedHashMap<String, Integer> linkedHashmap = new LinkedHashMap<String, Integer>();
+		linkedHashmap.put("özgür", 1);
+		linkedHashmap.put("kağan", 2);
+		linkedHashmap.put("ibrahim", 3);
+		linkedHashmap.put("barış", 4);
+		linkedHashmap.put("yunus", 5);
+
+		for (String gecici : linkedHashmap.keySet()) {
+			System.out.print(gecici + ", ");
+		}
+		System.out.println("\n\n");
+		for (int gecici : linkedHashmap.values()) {
+			System.out.print(gecici + ", ");
+		}
+		System.out.println("\n\n");
+		for (Entry<String, Integer> gecici : linkedHashmap.entrySet()) {
+			System.out.print(gecici + ", ");
+		}
+		System.out.println("\n\n");
+
+	}
 }
